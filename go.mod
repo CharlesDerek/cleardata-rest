237a947788662290ff645608e1e461fa493d28a8
charles derek <charlesderek177@gmail.com>
2022-12-15 17:21:46 -0500

    initializing flask variant

--- go.mod
+++
@@ -1,5 +0,0 @@
-module dev.mrgi-portal.com
-
-go 1.19
-
-require github.com/gorilla/mux v1.8.0
--- initialize-project.sh
+++ initialize-project.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Make go package (used dev.mrgi-portal.com as example domain).
-go mod init dev.mrgi-portal.com
-
-# Remove unused packages (if any exists).
-go mody tidy
-
-# String Conversions:
-go get -u strconv
+# Install Flask:
+pip install flask
